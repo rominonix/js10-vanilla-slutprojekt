05729a52bc7ad6a159e39e2e20708584be62a4a1
thomaswennhall <70705571+thomaswennhall@users.noreply.github.com>
2020-11-06 15:44:29 +0100

    uppdaterat JS
    med massor med loopar

--- js/main.js
+++ js/main.js
@@ -9,27 +9,47 @@ async function getRandomBeer(url){
 
 async function renderRandomBeer(beerData){
     renderRandomInfo(beerData)
+
+    renderListInfo(beerData)
+
     const beerImage = document.querySelector(".beer-image")
-    beerImage.src = beerData[0].image_url
+    
+    if(beerData[0].image_url == null){
+        beerImage.src = "/assets/missing.png"
+    } else{
+        beerImage.src = beerData[0].image_url
+    }
     const beerName = document.querySelector(".beer-name")
     beerName.innerHTML = beerData[0].name
 }
 
-// async function renderListInfo(beerData){
-//     let sortBeerData = []
-//     sortBeerData.push(beerData[0].name)
-//     sortBeerData.push(beerData[0].image_url)   
-//     sortBeerData.push(beerData[0].description)
-//     sortBeerData.push(beerData[0].abv)
+async function renderListInfo(beerData){
+    let sortBeerData = []
+    sortBeerData.push(beerData[0].abv)
+    sortBeerData.push(beerData[0].volume)
+    sortBeerData.push(beerData[0].ingredients)
+    sortBeerData.push(beerData[0].food_pairing)
+    sortBeerData.push(beerData[0].brewers_tips)
 
-// }
+    console.log(sortBeerData);
+    return sortBeerData
+}
+
+async function renderIngredients(beerData){
+
+}
 
 async function renderRandomInfo(beerData){
     const beerName = document.querySelector(".beer-name-info")
     beerName.innerHTML = beerData[0].name
 
     const beerImage = document.querySelector(".beer-image-info")
-    beerImage.src = beerData[0].image_url
+
+    if(beerData[0].image_url == null){
+        beerImage.src = "/assets/missing.png"
+    } else{
+        beerImage.src = beerData[0].image_url
+    }
 
     document.querySelector(".beer-info").innerHTML = ""
     document.querySelector(".description").innerHTML = ""
@@ -38,12 +58,42 @@ async function renderRandomInfo(beerData){
 
     document.querySelector(".description").append(descriptionItem)
 
-    for (let i = 6; i < 12; i++){
+    const sortBeerData = await renderListInfo(beerData)
+
+    for(let i = 0; i < sortBeerData.length; i++){
+        const newListItem = document.createElement("li")
+        
+        if(i == 0){
+            newListItem.innerHTML = `Alcohol by volume: ${sortBeerData[i]}` 
+        }else if(i == 1){
+            newListItem.innerHTML = `Volume: ${sortBeerData[i].value} ${sortBeerData[i].unit}` 
+        }else if(i == 2){
+            const newUnorderedList = document.createElement("ul")
+
+            newUnorderedList.innerHTML = "Ingredients: "
+
+            const newingredientsItem = document.createElement("li")
+            newingredientsItem.innerHTML = "Hops: "
+            newUnorderedList.append(newingredientsItem)
+
+            const hopsArray = sortBeerData[i].hops
+            hopsArray.forEach(hops => {
+                const newHopsItem = document.createElement("li")
+                newHopsItem.innerHTML = hops.name
+                newUnorderedList.append(newHopsItem)
+            });
+            newListItem.append(newUnorderedList)
+        }
+        
+        document.querySelector(".beer-info").append(newListItem)
+    }
+
+   /*  for (let i = 6; i < 12; i++){
         const newListItem = document.createElement("li")
         newListItem.innerHTML = beerData[0][i]
         console.log(beerData[0][i]);
         document.querySelector(".beer-info").append(newListItem)
-    }
+    } */
 }
 
 const moreInfoButton = document.querySelector(".more-info-button")
