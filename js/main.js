3a765303ee068ed01cc52b555120be7b632e6f7e
Romina Rebolledo <rominarebolledo@gmail.com>
2020-11-09 12:53:28 +0100

    Uppdaterat HTML / CSS / JS
    Organiserat JS
    Lagt till HTML
    Lagt till lite CSS

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//// HÄMTA INFO AV RANDOM BEER ////
 
 const randomBeerUrl = "https://api.punkapi.com/v2/beers/random"
 
@@ -6,22 +7,39 @@ async function getRandomBeer(url){
     const data = await response.json()
     return data
 }
+//// NAVIGATION ////
 
-async function renderRandomBeer(beerData){
-    renderRandomInfo(beerData)
-
-    renderListInfo(beerData)
-
-    const beerImage = document.querySelector(".beer-image")
-    
-    if(beerData[0].image_url == null){
-        beerImage.src = "/assets/missing.png"
-    } else{
-        beerImage.src = beerData[0].image_url
-    }
-    const beerName = document.querySelector(".beer-name")
-    beerName.innerHTML = beerData[0].name
+function navLinks(){
+    const links = document.querySelectorAll("nav > a")
+    for (let link of links){
+        link.addEventListener("click",()=>{
+            document.querySelectorAll("main > section").forEach(
+                section => section.classList.remove("active")
+            )
+            const section = document.querySelector("." + link.id)
+            section.classList.add("active")
+        })
+    }  
 }
+navLinks()
+
+//// BUTTONS HOME SIDA ////
+
+const moreInfoButton = document.querySelector(".more-info-button")
+const homePage = document.querySelector(".home")
+const beerInfoPage = document.querySelector(".info")
+moreInfoButton.addEventListener("click", function(){
+    homePage.classList.remove("active")
+    beerInfoPage.classList.add("active")
+})
+
+const randomBeerButton = document.querySelector(".random-button")
+randomBeerButton.addEventListener("click", async function(){
+    const beerData = await getRandomBeer(randomBeerUrl)
+    renderRandomBeer(beerData)   
+})
+
+//// ASYNC FUNCTIONS ////
 
 async function renderListInfo(beerData){
     let sortBeerData = []
@@ -30,52 +48,35 @@ async function renderListInfo(beerData){
     sortBeerData.push(beerData[0].ingredients)
     sortBeerData.push(beerData[0].food_pairing)
     sortBeerData.push(beerData[0].brewers_tips)
-
-    console.log(sortBeerData);
     return sortBeerData
-}
-
-async function renderIngredients(beerData){
-
 }
 
 async function renderRandomInfo(beerData){
     const beerName = document.querySelector(".beer-name-info")
     beerName.innerHTML = beerData[0].name
-
     const beerImage = document.querySelector(".beer-image-info")
-
     if(beerData[0].image_url == null){
         beerImage.src = "/assets/missing.png"
     } else{
         beerImage.src = beerData[0].image_url
     }
-
-    document.querySelector(".beer-info").innerHTML = ""
-    document.querySelector(".description").innerHTML = ""
     const descriptionItem = document.createElement("p")
     descriptionItem.innerHTML = beerData[0].description
-
     document.querySelector(".description").append(descriptionItem)
-
     const sortBeerData = await renderListInfo(beerData)
 
     for(let i = 0; i < sortBeerData.length; i++){
         const newListItem = document.createElement("li")
-        
         if(i == 0){
             newListItem.innerHTML = `Alcohol by volume: ${sortBeerData[i]}` 
         }else if(i == 1){
             newListItem.innerHTML = `Volume: ${sortBeerData[i].value} ${sortBeerData[i].unit}` 
         }else if(i == 2){
             const newUnorderedList = document.createElement("ul")
-
             newUnorderedList.innerHTML = "Ingredients: "
-
             const newingredientsItem = document.createElement("li")
             newingredientsItem.innerHTML = "Hops: "
             newUnorderedList.append(newingredientsItem)
-
             const hopsArray = sortBeerData[i].hops
             hopsArray.forEach(hops => {
                 const newHopsItem = document.createElement("li")
@@ -83,46 +84,20 @@ async function renderRandomInfo(beerData){
                 newUnorderedList.append(newHopsItem)
             });
             newListItem.append(newUnorderedList)
-        }
-        
+        }        
         document.querySelector(".beer-info").append(newListItem)
     }
-
-   /*  for (let i = 6; i < 12; i++){
-        const newListItem = document.createElement("li")
-        newListItem.innerHTML = beerData[0][i]
-        console.log(beerData[0][i]);
-        document.querySelector(".beer-info").append(newListItem)
-    } */
 }
 
-const moreInfoButton = document.querySelector(".more-info-button")
-const beerInfoPage = document.querySelector(".beer-info-page")
-moreInfoButton.addEventListener("click", function(){
-    hidSection()
-    beerInfoPage.classList.add("visible")
-
-})
-
-const randomBeerButton = document.querySelector(".random-button")
-randomBeerButton.addEventListener("click", async function(){
-    const beerData = await getRandomBeer(randomBeerUrl)
-    renderRandomBeer(beerData)
-   
-})
-
-const homePage = document.querySelector(".landing-page")
-const homeButton = document.querySelector(".home-button")
-homeButton.addEventListener("click", function(){
-    hidSection()
-    homePage.classList.add("visible")
-})
-
-
-function hidSection(){
-    const allSections = document.querySelectorAll("section")
-    for (let section of allSections) {
-        section.classList.add("hidden")
-        section.classList.remove("visible")
+async function renderRandomBeer(beerData){
+    renderRandomInfo(beerData)
+    renderListInfo(beerData)
+    const beerImage = document.querySelector(".beer-image")
+    if(beerData[0].image_url == null){
+        beerImage.src = "/assets/missing.png"
+    } else{
+        beerImage.src = beerData[0].image_url
     }
+    const beerName = document.querySelector(".beer-name")
+    beerName.innerHTML = beerData[0].name
 }
